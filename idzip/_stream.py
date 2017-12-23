d4428960991ec67efccf8866f62716fcbcee0709
Joshua Klein <mobiusklein@gmail.com>
2017-12-23 10:34:31 -0500

    utility code centralized

--- idzip/_stream.py
+++ idzip/_stream.py
@@ -15,6 +15,16 @@ class IOStreamWrapperMixin(object):
     def writable(self):
         return self.stream.writable()
 
+    def fileno(self):
+        return self.stream.fileno()
+
     def __del__(self):
         if not self.closed:
             self.close()
+
+
+def check_file_like_for_writing(f):
+    check = (
+        hasattr(f, "write") and hasattr(f, "tell") and
+        hasattr(f, 'flush') and hasattr(f, 'close)'))
+    return check
