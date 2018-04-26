fc5ccbc5348ec9dc07d00958c40c4f610512522f
dan <bauman.85@osu.edu>
2018-04-26 18:42:53 -0500

    bump to 0.3

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python
 from setuptools import setup, find_packages
 
+VERSION = "0.3"
+
 setup(
     name = "python-idzip",
-    version = "0.1",
+    version = VERSION,
     packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "idzip = idzip.command:main"
-        ]
-    }
+    description = 'DictZip - Random Access gzip files',
+    author = 'Rik Faith',
+    maintainer= 'Dan Bauman',
+    maintainer_email='dan@bauman.space',
+    license='MIT',
+    url = 'https://github.com/bauman/python-idzip',
+    download_url = 'https://github.com/bauman/python-idzip/%s.tar.gz' %(VERSION),
+    classifiers = [
+                       'License :: OSI Approved :: MIT License',
+                       'Operating System :: OS Independent',
+                       'Programming Language :: Python',
+                       'Programming Language :: Python :: 2',
+                       'Programming Language :: Python :: 3',
+                   ],
+    scripts=['bin/idzip']
 )
